233c4060871d2d950eed1cd26c437805f43c177f
Paul <paulventer94@gmail.com>
2020-05-12 15:17:10 +0300

    Initial commit

---
+++ src/main/java/org/fasttractit/curs23homework/Curs23HomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.fasttractit.curs23homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs23HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curs23HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttractit/curs23homework/Curs23HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttractit.curs23homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curs23HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
